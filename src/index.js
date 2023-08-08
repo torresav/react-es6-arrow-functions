00793f16568756b2ecf7db0a74af7781619873d2
Aaron Torres <torresaa92@gmail.com>
2023-08-08 12:44:01 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// var numbers = [3, 56, 2, 48, 5];
+
+// const newNumbers = numbers.map(x => x * x);
+// console.log(newNumbers);
+
+var numbers = [3, 56, 2, 48, 5];
+
+//Map -Create a new array by doing something with each item in an array.
+// const newNumbers = numbers.map(x => x * 2);
+
+////Filter - Create a new array by keeping the items that return true.
+//const newNumbers = numbers.filter(num => num < 10);
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+//var newNumbers = numbers.reduce((accumulator, currentNumber) => (accumulator + currentNumber));
+
+////Find - find the first item that matches from an array.
+//const newNumbers = numbers.find(num => num > 10)
+
+////FindIndex - find the index of the first item that matches.
+const newNumbers = numbers.findIndex(num => num > 10)
+
+console.log(newNumbers);
